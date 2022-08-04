aacb2d446bd90ea5c95fa364b1ebb0662974b052
mirnaqu23-meet <109899438+mirnaqu23-meet@users.noreply.github.com>
2022-08-04 12:34:19 +0300

    My Cart is not done and idk how to do it

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -12,8 +12,7 @@ firebaseConfig = {
   "storageBucket": "individual-project-faf84.appspot.com",
   "messagingSenderId": "38185549529",
   "appId": "1:38185549529:web:db3926c1732149819888c8",
-  "measurementId": "G-TPL435XZFR",
-  "databaseURL" :"https://individual-project-faf84-default-rtdb.europe-west1.firebasedatabase.app/"
+  "measurementId": "G-TPL435XZFR"
 }
 
 firebase = pyrebase.initialize_app(firebaseConfig)
@@ -27,41 +26,63 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 
 @app.route('/signin', methods=['GET', 'POST']) 
 def signin():
-   error = ""
-   if request.method == 'POST':
-       email = request.form['email']
-       password = request.form['password']
-       try:
-            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
-            return redirect(url_for('signin'))
-       except:
-           error = "Authentication failed"
-   return render_template("signin.html")
+	error = ""
+	if request.method == 'POST':
+		email = request.form['email']
+		password = request.form['password']
+		try:
+			login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+			return redirect(url_for('index'))
+		except:
+			error = "Authentication failed"
+			return error
+	else:
+		return render_template("signin.html")
 
 
 @app.route('/signup', methods= ['GET','POST'])
 def signup():
-    error= ""
-    if request. method == 'POST':
-        email = request.form ['email']
-        password = request. form ['password']
-        full_name = request.form ['text']
-        try:
-            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
-            user= {'full_name' : full_name}
-            db.child("Users").child(login_session['user']['lacalId']).set(user)
-            return redirect(url_for('home'))
-        except:
-           error = "Authentication failed"
-    return render_template('signup.html')
+	error= ""
+	if request. method == 'POST':
+		email = request.form ['email']
+		password = request. form ['password']
+		full_name = request.form ['First_Name']
+		try:
+			login_session['user'] = auth.create_user_with_email_and_password(email, password)
+			user= {"full_name" : full_name, "email": email, "password":password}
+			db.child("Users").child(login_session['user']['localId']).set(user)
+			return redirect(url_for('homepage'))
+		except:
+			error = "Authentication failed"
+			return error
+	else:
+		return render_template('signup.html')
 
+@app.route('/index') 
+def index():
+		return render_template("index.html")
 
 @app.route('/')
 def homepage():
-    return render_template('home.html')
+	return render_template('home.html')
 
+@app.route('/beauty')
+def beautypage():
+	return render_template('beauty.html')
+
+@app.route('/bags')
+def bagpage():
+	return render_template('bags.html')
+
+@app.route('/men')
+def menpage():
+	return render_template('men.html')
+
+@app.route('/cart')
+def cartpage():
+	return render_template('Cart.html')
 
 #Code goes above here
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
